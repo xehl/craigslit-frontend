910393aa8ea697b4fa7b0e6d06eb1f356ce840ac
ehlee95 <eric95lee@gmail.com>
2022-05-17 14:34:17 -0400

    finished submission form

--- src/screens/submitform.jsx
+++ src/screens/submitform.jsx
@@ -6,7 +6,33 @@ export default function SubmitForm(props) {
   return (
     <div className="submit-container">
       <div className="submit-header">
-        <button>craigslit</button>
+        <button className="home-button">cl</button>
+        <div>craigslit > post</div>
+      </div>
+      <div className="form-container">
+        <div className="top-row">
+          <div>
+              {/* <label for="title">title</label> */}
+              <input type="text" placeholder="title" id="title" className="input"/>
+              {/* <label for="author">author</label> */}
+              <input type="text" placeholder="author" id="author" className="input" />
+          </div>
+          <div>
+            <span>$ <input type="text" placeholder="price" id="price" className="input" /></span>
+            <input type="text" placeholder="location" id="location" className="input"/>
+          </div>
+        </div>
+        <textarea className="description" placeholder="Description"/>
+        <div className="detail-container">
+          <div className="bottom-row">
+            <div>
+              <input type="text" placeholder="condition" id="condition" className="input"/>
+              <input type="text" placeholder="size/dimensions" id="size" className="input"/>
+              <input type="text" placeholder="other notes" id="notes" className="input" />
+            </div>
+            <button className="submit-button">submit listing</button>
+          </div>
+        </div>
       </div>
     </div>
   );
